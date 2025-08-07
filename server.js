000fbf4a946ee7388be5b96858d1c741bc9d8e8c
TejasJagadale <139218507+TejasJagadale@users.noreply.github.com>
2025-08-07 15:13:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 // Enable CORS
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"], 
+    origin: ["http://localhost:3000", "http://localhost:3001", "https://todayjobs.vercel.app"], 
     credentials: true
   })
 );
@@ -35,3 +35,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
